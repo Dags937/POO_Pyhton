8ba26cf9d3229c4d27e61675c375281ae7df52d9
Dags937 <37696816+Dags937@users.noreply.github.com>
2025-12-10 07:20:11 -0500

    Update index.py

--- actividad_botellas/index.py
+++ actividad_botellas/index.py
@@ -1,10 +1,15 @@
 from padre import Botella
 from hijo1 import Botella_plastica
 from hijo2 import Botella_vidrio
+from base_datos import Base_datos
 
 #codigo principal
+obj_base_datos=Base_datos()
 #hacer el objeto
-obj_botella=Botella("Metal", "1 Litro", "Conica")
+material=input("Seleccione el material de la botella:")
+capacidad=input("La capacidad de la botella es:")
+forma=input("La forma de la botella es :")
+obj_botella=Botella(material, capacidad, forma)
 obj_botella.material #atributo
 dato_atributo = obj_botella.get_material()
 dato_liquido = obj_botella.contener_liquido("Nestea")
@@ -18,3 +23,7 @@ obj_botella_vidrio=Botella_vidrio("Vidrio", "2 litros", "circular", "templado", 
 imprimir_vidrio=obj_botella_vidrio.imprimir_datos()
 
 print(imprimir_vidrio)
+
+base_datos.guardar_objeto(obj_botella)
+
+
